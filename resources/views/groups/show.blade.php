699cfba1d78c317ade51a1f5d771d353f432a2b7
cianf4 <alessandrocianf@gmail.com>
2023-10-07 16:19:07 +0200

    Pulsante modifica funzionante! Da modificare gli stili

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -4,19 +4,24 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex justify-between items-center">
-                        <h2 class="text-2xl font-semibold mb-2" id="group-name">{{ $group->name }}</h2>
+                        <h2 class="text-2xl font-semibold mb-2" id="group-name-display">{{ $group->name }}</h2>
                         @if (auth()->user()->isAdmin())
                             <div class="ml-auto">
                                 <button id="editGroupNameButton" class="bg-gray-600 hover:bg-gray-900 text-white font-semibold py-1 px-2 rounded shadow-md text-xs uppercase">
                                     Modifica
+                                </button>
+                                <button id="saveGroupName" class="bg-gray-600 hover:bg-gray-900 text-white font-semibold py-1 px-2 rounded shadow-md text-xs uppercase hidden">
+                                    Salva
                                 </button>
                             </div>
                         @endif
                     </div>
                     <div class="hidden" id="editGroupNameForm">
                         <input type="text" id="newGroupName" value="{{ $group->name }}" class="border rounded p-1">
-                        <button id="saveGroupName" class="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 text-xs uppercase">Salva</button>
                     </div>
+
+
+
                     <p class="text-gray-600 mb-4">
                         {{ $group->description }}
                     </p>
@@ -43,16 +48,22 @@
 </x-app-layout>
 
 <script>
-    document.getElementById('editGroupNameButton').addEventListener('click', function () {
-        document.getElementById('group-name').style.display = 'none';
-        document.getElementById('editGroupNameForm').style.display = 'block';
+    const editButton = document.getElementById('editGroupNameButton');
+    const saveButton = document.getElementById('saveGroupName');
+    const editGroupNameForm = document.getElementById('editGroupNameForm');
+    const groupNameDisplay = document.getElementById('group-name-display');
+
+    editButton.addEventListener('click', function () {
+        groupNameDisplay.style.display = 'none';
+        editGroupNameForm.style.display = 'block';
+        editButton.style.display = 'none';
+        saveButton.style.display = 'block';
     });
 
-    document.getElementById('saveGroupName').addEventListener('click', function () {
+    saveButton.addEventListener('click', function () {
         const newGroupName = document.getElementById('newGroupName').value;
-        const groupId = {{ $group->id }}; // Sostituisci con l'ID del gruppo
+        const groupId = {{ $group->id }};
 
-        // Esegui una richiesta AJAX per aggiornare il nome del gruppo
         fetch(`/groups/${groupId}`, {
             method: 'PUT',
             headers: {
@@ -63,12 +74,14 @@
         })
             .then((response) => response.json())
             .then((data) => {
-                // Aggiorna il nome visualizzato
-                document.getElementById('group-name').textContent = newGroupName;
+                groupNameDisplay.textContent = newGroupName;
+                //groupNameDisplay.style.fontWeight = 'bold';
+                //groupNameDisplay.style.color = '#000';
 
-                // Nascondi il campo di input e il pulsante "Salva"
-                document.getElementById('group-name').style.display = 'block';
-                document.getElementById('editGroupNameForm').style.display = 'none';
+                editGroupNameForm.style.display = 'none';
+                editButton.style.display = 'block';
+                saveButton.style.display = 'none';
+                groupNameDisplay.style.display ='block';
             })
             .catch((error) => {
                 console.error('Errore durante l\'aggiornamento del nome del gruppo:', error);

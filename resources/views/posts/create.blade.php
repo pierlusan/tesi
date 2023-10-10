8a18e4acd385bfcd3d8c180c8c453cbbe7865507
cianf4 <alessandrocianf@gmail.com>
2023-10-10 19:38:53 +0200

    Aggiornati gli stili di posts/create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,21 +1,30 @@
 <x-app-layout>
-    <div class="max-w-lg mx-auto mt-10 p-6 bg-white rounded shadow-md">
-        <h1 class="text-2xl font-semibold mb-6">Crea un nuovo post</h1>
+    <div class="py-6">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <x-group-menu  :group="$group" />
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1 class="text-2xl font-semibold mb-4">{{ $group->name }} - Crea Post</h1>
 
-        <form method="POST" action="{{ route('posts.store', ['group' => $group]) }}" class="space-y-4">
-            @csrf
-            <div class="mb-4">
-                <label for="title" class="block text-gray-600">Titolo</label>
-                <!--<input type="hidden" name="group_id" value="{{ $group->id }}">-->
-                <input type="text" name="title" id="title" class="w-full border border-gray-300 rounded px-4 py-2 focus:border-blue-500 focus:ring focus:ring-blue-200" required>
+                    <form method="POST" action="{{ route('posts.store', ['group' => $group]) }}" class="space-y-4">
+                        @csrf
+                        <div class="mb-4">
+                            <label for="title" class="block text-gray-600">Titolo</label>
+                            <!--<input type="hidden" name="group_id" value="{{ $group->id }}">-->
+                            <x-text-input type="text" name="title" class="block w-full" id="title" required></x-text-input>
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="content" class="block text-gray-600">Contenuto</label>
+                            <textarea name="content" id="content" class="block h-32 mt-1 mb-4 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" required></textarea>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-primary-button type="submit">Crea Post</x-primary-button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <div class="mb-4">
-                <label for="content" class="block text-gray-600">Contenuto</label>
-                <textarea name="content" id="content" class="w-full border border-gray-300 rounded px-4 py-2 focus:border-blue-500 focus:ring focus:ring-blue-200" required></textarea>
-            </div>
-
-            <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">Crea Post</button>
-        </form>
+        </div>
     </div>
 </x-app-layout>

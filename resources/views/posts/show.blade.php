a1bb6208c1234d3d5fae0e4f17fed5c54675cb16
cianf4 <alessandrocianf@gmail.com>
2023-10-18 22:17:44 +0200

    Aggiunto header in ogni pagina

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,10 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Gruppi - ') }} <span class="font-normal">{{ $group->name }}</span>
+        </h2>
+    </x-slot>
+
     <div class="py-6">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">

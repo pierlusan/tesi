044615c63cfe510f6d79c06397ca53b53a726d7f
cianf4 <alessandrocianf@gmail.com>
2023-11-13 09:59:42 +0100

    Aggiornati stili di room e lobby

--- resources/views/single_events/lobby.blade.php
+++ resources/views/single_events/lobby.blade.php
@@ -1,13 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Evento Singolo') }}
-        </h2>
-    </x-slot>
-    <div class="py-6">
+    <div class="py-16">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="backdrop-blur-3xl overflow-hidden shadow-md sm:rounded-lg">
+                <div class="p-6">
                     <div id="videos">
                         <video class="video-player"  id="user-1" autoplay playsinline></video>
                         <video class="video-player"  id="user-2" autoplay playsinline></video>

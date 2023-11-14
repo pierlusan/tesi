46bc4b29742c28261146497e2346767f6a4f1133
cianf4 <alessandrocianf@gmail.com>
2023-11-14 16:50:44 +0100

    Modificata la pagina della registrazione in approvazione

--- resources/views/approval_request.blade.php
+++ resources/views/approval_request.blade.php
@@ -1,17 +1,9 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Richiesta di Approvazione') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 text-gray-900">
-                    <p class="text-lg">{{ __("Grazie per la tua registrazione.") }}</p>
-                    <p class="mt-4 text-gray-600">{{ __("La tua richiesta di approvazione è stata inoltrata. Sarai informato non appena verrai approvato.") }}</p>
-                </div>
+            <div class="backdrop-blur-300 p-6 overflow-hidden shadow-md sm:rounded-lg">
+                <p class="text-lg text-stone-100">{{ __("Grazie per la tua registrazione!") }}</p>
+                <p class="mt-4 text-stone-300">{{ __("La tua richiesta di approvazione è stata inoltrata. Sarai informato non appena verrai approvato.") }}<br>{{ __("Una volta approvato, potrai accedere all'applicazione.") }}</p>
             </div>
         </div>
     </div>
